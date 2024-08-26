9739b67e083c13123604b66fb250fe52804583f6
Dekucuk <86988600+YusufDekucuk@users.noreply.github.com>
2024-08-26 23:29:05 +0300

    Delete example.lua

--- example.lua
+++
@@ -1,8 +0,0 @@
-RegisterCommand('testlockpick', function() 
-    local minigame = exports["qb-lockpick"]:Minigame()
-    if minigame then 
-        print('User won the minigame')
-    else
-        print('User lost the minigame')
-    end
-end)
